b869d4f6bbd1f941cb4171399ca2a6ccdfd78f53
tylerpiercey <122289319+tylerpiercey@users.noreply.github.com>
2023-11-14 21:38:22 -0330

    Created input boxes for text prompts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  Const [textPrompt, setTextPrompt] = usestate('');
+  const [negativeTextPrompt, setNegativeTextPrompt] = useState('');
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +12,20 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input
+          type="text"
+          value={textPrompt}
+          onChange={(e) => setTextPrompt(e.target.value)}
+          placeholder="Enter text prompt"
+        />
+        <input
+          type="text"
+          value={negativeTextPrompt}
+          onChange={(e) => setNegativeTextPrompt(e.target.value)}
+          placeholder="Enter negative text prompt"
+        />
+        <p>Text Prompt: {textPrompt}</p>
+        <p>Negative Text Prompt: {negativeTextPrompt}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
