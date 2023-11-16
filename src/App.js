981cb5ade85a6f59f30ec2b8c41b3f991953e22d
tylerpiercey <122289319+tylerpiercey@users.noreply.github.com>
2023-11-15 20:54:56 -0330

    Added text input boxes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-=======
-import React, {useState, useEffect} from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap"
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ControlPanel from './components/ControlPanel'
->>>>>>> 658eee91e5531fcd051a65362e4e138b948230f1
+import ControlPanel from './components/ControlPanel';
 
 function App() {
-  Const [textPrompt, setTextPrompt] = usestate('');
-  const [negativeTextPrompt, setNegativeTextPrompt] = useState('');
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input
-          type="text"
-          value={textPrompt}
-          onChange={(e) => setTextPrompt(e.target.value)}
-          placeholder="Enter text prompt"
-        />
-        <input
-          type="text"
-          value={negativeTextPrompt}
-          onChange={(e) => setNegativeTextPrompt(e.target.value)}
-          placeholder="Enter negative text prompt"
-        />
-        <p>Text Prompt: {textPrompt}</p>
-        <p>Negative Text Prompt: {negativeTextPrompt}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-  <Container> 
-    <Router>
-      <Routes>
-        <Route path='/*' element={<ControlPanel />} />
-      </Routes>  
-    </Router>
-  </Container>  
->>>>>>> 658eee91e5531fcd051a65362e4e138b948230f1
+    <Container> 
+      <Router>
+        <Routes>
+          <Route path='/*' element={<ControlPanel />} />
+        </Routes>  
+      </Router>
+    </Container>  
   );
 }
 
--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 
-
 function ControlPanel() {
+  const [textPrompt, setTextPrompt] = useState('');
+  const [negativeTextPrompt, setNegativeTextPrompt] = useState('');
+
   return (
     <Container>
       <Form.Group data-bs-theme="dark">  
@@ -15,6 +17,28 @@ function ControlPanel() {
             <option value="2">Midjourney</option>
             <option value="3">Dall-E 3.0</option>
         </Form.Select>
+
+        {/* Text Prompt Input */}
+        <Form.Group className="mb-3">
+          <Form.Label>Text Prompt</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Enter text prompt" 
+            value={textPrompt}
+            onChange={(e) => setTextPrompt(e.target.value)}
+          />
+        </Form.Group>
+
+        {/* Negative Text Prompt Input */}
+        <Form.Group className="mb-3">
+          <Form.Label>Negative Text Prompt</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Enter negative text prompt" 
+            value={negativeTextPrompt}
+            onChange={(e) => setNegativeTextPrompt(e.target.value)}
+          />
+        </Form.Group>
       </Form.Group>
     </Container>
   );
