f8732cb49973427b7fee966085ccce72e429752c
memerax <josephwhite@gmx.com>
2023-11-27 19:02:32 -0330

    Testing Express server Basics

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
+import req from "express/lib/request.js";
+import res from "express/lib/response.js";
 
 
 const app = express()
-const port = 4000;
+const port = 3000
+
+app.get("/hello", (req, res) => {
+    res.send('hello')
+})
+
+app.listen(port, () =>{
+    console.log('Server is listening')
+})
