1ce44cefd2ada0326f5b111a8bac77e0beb6a100
memerax <josephwhite@gmx.com>
2023-11-27 18:58:34 -0330

    Updated Package.json files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
-import { fileURLToPath } from 'url';
-import path from 'path';
 import 'dotenv/config';
 
 
