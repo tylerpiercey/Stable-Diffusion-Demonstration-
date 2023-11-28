ea39b61322271c36bde25e895c2c31da680bde4d
memerax <josephwhite@gmx.com>
2023-11-27 21:44:12 -0330

    Posting image links - postman tested - works

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {response} from 'express';
 // import 'dotenv/config';
 // import req from "express/lib/request.js";
 // import res from "express/lib/response.js";
@@ -15,6 +15,16 @@ app.get('/api/images', async (req,res) => {
     const db = client.db('asd-data')
     const images = await db.collection('images').find({}).toArray()
     res.json(images)
+})
+
+app.post("/api/saveimage/", async (req,res,response) =>{
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+    await client.connect()
+    const db = client.db('asd-data')
+    let imagelink = req.body.link
+    res.send(imagelink)
+    await db.collection("images").insertOne({imagelink})
+
 })
 
 app.listen(port, () =>{
