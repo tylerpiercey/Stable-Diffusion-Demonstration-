4d53d5fc04fd3b2dbb2feaca1f4f5fa34bcd46b1
memerax <josephwhite@gmx.com>
2023-11-27 20:33:33 -0330

    Added image route to database

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import 'dotenv/config';
+// import 'dotenv/config';
 import req from "express/lib/request.js";
 import res from "express/lib/response.js";
+import {MongoClient} from "mongodb";
 
 
 const app = express()
@@ -10,6 +11,14 @@ const port = 3000
 
 app.get("/hello", (req, res) => {
     res.send('hello')
+})
+
+app.get('/api/images', async (req,res) => {
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+    await client.connect()
+    const db = client.db('asd-data')
+    const images = await db.collection('images').find({}).toArray()
+    res.json(images)
 })
 
 app.listen(port, () =>{
